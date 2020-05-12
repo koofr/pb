1389bea13f8d4dd67e73ad084563b05eef36d06a
Luka Zakrajšek <luka@bancek.net>
2020-05-12 10:47:36 +0200

    Remove w32 dependency

--- pb_win.go
+++ pb_win.go
@@ -2,20 +2,10 @@
 
 package pb
 
-import (
-	"syscall"
-
-	"github.com/AllenDang/w32"
-)
-
 func bold(str string) string {
 	return str
 }
 
 func terminalWidth() (int, error) {
-	screenBufInfo := w32.GetConsoleScreenBufferInfo(w32.HANDLE(syscall.Stdout))
-	if screenBufInfo == nil {
-		return 79, nil
-	}
-	return int(screenBufInfo.DwSize.X) - 1, nil
+	return 79, nil
 }
